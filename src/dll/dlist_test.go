7520f2c2654554007b4fd11a3fe7e7016482e2de
Mahidas Prabhanjan <mahidas.prabhanjan@hpe.com>
2020-09-12 20:13:35 -0700

    add more testcases to dlist

--- src/dll/dlist_test.go
+++ src/dll/dlist_test.go
@@ -131,3 +131,81 @@ func TestDlistPopTail(t *testing.T) {
         i--
     }
 }
+func TestDlistResinsertAtHead(t *testing.T) {
+
+    elements := make([]int, 100000)
+    dlist := NewDlist()
+    var link *Dlist_link
+    for i := 0; i < 100000; i++ {
+        elements[i] = 100+i
+        if i == 50000 {
+            link = dlist.InsertAtTail(elements[i])
+        } else {
+            dlist.InsertAtTail(elements[i])
+        }
+    }
+
+    dlist.ReInsertAtHead(link)
+    newElems := elements[50000:50001]
+    newElems = append(newElems, elements[:50000]...)
+    newElems = append(newElems, elements[50001:]...)
+
+    iter := NewDlistIterator(dlist)
+    i := 0
+    for el := iter.Next(); el != nil; el = iter.Next() {
+        val := el.(int)
+        if val != newElems[i] {
+            t.Errorf("TestDlistReinsertAtHead(): Elem %d not at correct order %d!!", val, i)
+        }
+        i++
+    }
+
+    iter = NewDlistIterator(dlist)
+    i = len(elements)-1
+    for el := iter.ReverseNext(); el != nil; el = iter.ReverseNext() {
+        val := el.(int)
+        if val != newElems[i] {
+            t.Errorf("TestDlistReinsertAtHead() - Reversewalk: Elem %d not at correct order %d!!", val, i)
+        }
+        i--
+    }
+}
+func TestDlistResinsertAtTail(t *testing.T) {
+
+    elements := make([]int, 100000000)
+    dlist := NewDlist()
+    var link *Dlist_link
+    for i := 0; i < 100000000; i++ {
+        elements[i] = 100+i
+        if i == 99999 {
+            link = dlist.InsertAtTail(elements[i])
+        } else {
+            dlist.InsertAtTail(elements[i])
+        }
+    }
+
+    dlist.ReInsertAtTail(link)
+    newElems := elements[:99999]
+    newElems = append(newElems, elements[99999+1:]...)
+    newElems = append(newElems, elements[99999])
+
+    iter := NewDlistIterator(dlist)
+    i := 0
+    for el := iter.Next(); el != nil; el = iter.Next() {
+        val := el.(int)
+        if val != newElems[i] {
+            t.Errorf("TestDlistReinsertAtHead(): Elem %d not at correct order %d!!", val, i)
+        }
+        i++
+    }
+
+    iter = NewDlistIterator(dlist)
+    i = len(elements)-1
+    for el := iter.ReverseNext(); el != nil; el = iter.ReverseNext() {
+        val := el.(int)
+        if val != newElems[i] {
+            t.Errorf("TestDlistReinsertAtHead() - Reversewalk: Elem %d not at correct order %d!!", val, i)
+        }
+        i--
+    }
+}
